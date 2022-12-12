ccc61f14016a925757427c796577213ff6e6c941
flyer3742 <64683728+flyer3742@users.noreply.github.com>
2022-12-12 05:51:27 -0500

    Update magic8.py
    Changed code to update statement if user does not provide a name or question. Work still needs to be done on printing 1 question and answer on output.

--- magic8.py
+++ magic8.py
@@ -1,9 +1,22 @@
+#Randomizer to randomly selects a number 
 import random
 random_number = random.randint(1, 12)
 print(random_number)
 name = "Eric"
-question = "Will it rain tomorrow?"
-answer = "Yes" or "No"
+question = "Will I win the lottery?"
+answer = ""
+#If user does not provide a name code below will run
+if len(name) == 0:
+  print("Question: " + question)
+else:
+  print(name + " asks: " + question)
+#If user does not provide a question code below will run
+if question == "":
+  print("The Magic 8-Ball cannot provide a fortune unless you ask it something.")
+else:
+  print(name + " asks: " + question)
+  print("Magic 8-Ball's answer: " + answer)
+# if statements 1 - 12 and Error as else
 if random_number == 1:
   answer = "Yes - definitely."
 elif random_number == 2:
@@ -30,6 +43,4 @@ elif random_number == 12:
   answer = "You can count on it!"
 else:
    answer = "Error"
-print(name + " asks: " + question)
 print("Magic 8-Ball's answer: " + answer)
-
